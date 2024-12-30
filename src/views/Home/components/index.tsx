9be0290804a186a03ddb8a73a5127d105977a622
solaiman-naderi <solaimannaderi00@gmail.com>
2024-12-30 11:11:23 +0330

    Feat: added useFetch custome hook for call api

--- src/views/Home/components/index.tsx
+++ src/views/Home/components/index.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import useFetch from "@/hooks/useFetch";
+
 const Slider = () => {
-  return <>Slider</>;
+  const sliderData = useFetch("/photos?_start=0&_limit=10");
+  console.log(sliderData);
+
+  return <div className="min-h-screen w-screen bg-red-50">d</div>;
 };
 export default Slider;
